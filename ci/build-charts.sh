dee1a8912803b5b313fbb67249381d710a327932
Philipp Heuer <git@philippheuer.me>
2019-03-10 22:40:24 +0100

    chore: use `envcli run` prefix for commands

--- ci/build-charts.sh
+++ ci/build-charts.sh
@@ -7,7 +7,7 @@ if [ -d "${CHART_DIR}" ]; then
     # Build Each Chart
     echo "Now building chart ${CHART_DIR} ...";
     cd ${CHART_DIR};
-    helm package ./;
+    envcli run helm package ./;
     mv *.tgz $CI_PROJECT_DIR/public;
 fi
 done
