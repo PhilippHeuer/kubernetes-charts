88eb09c1b53d3c2825e88672a5b969e6bafbc0d1
Philipp Heuer <git@philippheuer.me>
2019-03-10 23:29:21 +0100

    fix: helm package needs to relative path

--- ci/build-charts.sh
+++ ci/build-charts.sh
@@ -5,8 +5,7 @@ echo "Now building all charts in ${REPO_DIR}"
 for CHART_DIR in $REPO_DIR/*; do
 if [ -d "${CHART_DIR}" ]; then
     # Build Each Chart
-    echo "Now building chart ${CHART_DIR} ...";
-    helm package ${CHART_DIR};
-    mv ${CHART_DIR}/*.tgz $CI_PROJECT_DIR/public;
+    echo "Now building chart ${CHART_DIR#"$CI_PROJECT_DIR/"} ..."
+    helm package ${CHART_DIR#"$CI_PROJECT_DIR/"} --destination ./public
 fi
 done
