8641c1a6424383691108dfc3b87b62bff2c55548
Philipp Heuer <git@philippheuer.me>
2019-03-10 22:46:51 +0100

    chore: optimize ci build script

--- ci/build-charts.sh
+++ ci/build-charts.sh
@@ -6,8 +6,7 @@ for CHART_DIR in $REPO_DIR/*; do
 if [ -d "${CHART_DIR}" ]; then
     # Build Each Chart
     echo "Now building chart ${CHART_DIR} ...";
-    cd ${CHART_DIR};
-    envcli run helm package ./;
-    mv *.tgz $CI_PROJECT_DIR/public;
+    helm package ${CHART_DIR};
+    mv ${CHART_DIR}/*.tgz $CI_PROJECT_DIR/public;
 fi
 done
